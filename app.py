d829dace0e5f1f87be6528ffd8d5a6dfc6afd15a
Oleksandr Markov <ol.markov1999@gmail.com>
2021-02-26 00:18:27 +0200

    some today`s params + short forecast for tomorrow

---
+++ app.py
@@ -0,0 +1,23 @@
+from pyowm import OWM
+
+# api key from 'https://home.openweathermap.org/api_keys'
+token = 'e981ce5d4ca812a48de1d85d33a5a2f0'
+
+owm = OWM(token)
+mgr = owm.weather_manager()
+
+observation = mgr.weather_at_place('Запорожье')
+
+w = observation.weather
+wind = observation.weather.wind(unit='miles_hour')   # Default unit: 'meters_sec'
+
+tomorrow = mgr.forecast_at_place('Запорожье', '3h')
+
+print('Status: ' + w.detailed_status)
+print('Current temperature (°C): ' + str(w.temperature('celsius')['temp']))
+print('Wind`s speed (miles/hour): ' + str(round(wind['speed'], 2)))
+print('Humidity: ' + str(w.humidity) + '%')
+print('Cloudiness: ' + str(w.clouds) + '%')
+print()
+print('Is it going to snow tomorrow? ' + str(tomorrow.will_have_snow()))
+print('Is it going to fog tomorrow? ' + str(tomorrow.will_have_fog()))
